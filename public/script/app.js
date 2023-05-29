0166ca88e878918ffc0ccef4f4c1b1f71a73a475
Michael Potter <michaeljpotter@live.com>
2023-05-28 20:12:03 -0700

    Okay... its something at least.

--- public/script/app.js
+++ public/script/app.js
@@ -10,76 +10,127 @@ const FBX_FILE_OFFICE = 'Block-Office.fbx';
 const FBX_FILE_SHOPPING_1 = 'Block-Shopping-1.fbx';
 const FBX_FILE_SHOPPING_2 = 'Block-Shopping-2.fbx';
 const FBX_FILE_STATION = 'Block-Station.fbx';
+const FBX_FILES = [
+	FBX_FILE_APARTMENTS, FBX_FILE_OFFICE,
+	FBX_FILE_SHOPPING_1, FBX_FILE_SHOPPING_2, FBX_FILE_STATION
+];
+
+window.THREE = THREE;
 
 var FogHornBoot = function () {
-	const scene = new THREE.Scene()
-	scene.add(new THREE.AxesHelper(5))
-
+	this.scene = new THREE.Scene()
+	this.scene.add(new THREE.AxesHelper(5))
 	
-	const light = new THREE.PointLight()
-	light.position.set(0.8, 1.4, 1.0)
-	scene.add(light)
+	this.light = new THREE.PointLight()
+	this.light.position.set(0.8, 1.4, 1.0)
+	this.scene.add(this.light)
 
-	const ambientLight = new THREE.AmbientLight()
-	scene.add(ambientLight)
-	const camera = new THREE.PerspectiveCamera(
+	this.ambientLight = new THREE.AmbientLight()
+	this.scene.add(this.ambientLight)
+	this.camera = new THREE.PerspectiveCamera(
 		75,
 		window.innerWidth / window.innerHeight,
 		0.1,
-		1000
+		20000
 	)
-	camera.position.set(0.8, 1.4, 1.0)
+	this.camera.position.set(0.8, 1.4, 1.0)
 	
-	const renderer = new THREE.WebGLRenderer()
-	renderer.setSize(window.innerWidth, window.innerHeight)
-	document.body.appendChild(renderer.domElement)
+	this.renderer = new THREE.WebGLRenderer()
+	this.renderer.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(this.renderer.domElement)
 
-	const controls = new OrbitControls(camera, renderer.domElement)
-	controls.enableDamping = true
-	controls.target.set(0, 1, 0)
+	this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+	this.controls.enableDamping = true
+	this.controls.target.set(0, 1, 0)
 	
+	this.sky = new THREE.Mesh(
+        new THREE.SphereGeometry(10000, 32, 32),
+        new THREE.MeshBasicMaterial({
+            color: 0x8080FF,
+            side: THREE.BackSide,
+        })
+    );
+    this.scene.add(this.sky);
+	
+	this.blocks = [];
 	const fbxLoader = new FBXLoader()
-	fbxLoader.load(
-		FBX_FILE_APARTMENTS,
+	
+	this.blockSize
+	this.SetupBlocks = () => {
+		let box3 = new THREE.Box3().setFromObject( this.blocks[0] );
+		let size = new THREE.Vector3();
+		box3.getSize(size);
+		this.blockSize = box3;
+		
+		// assume assets are same size for now;
+		Object.assign (this.blocks [0].position, new THREE.Vector3(0, 0, 0));
+		Object.assign (this.blocks [1].position, new THREE.Vector3(-box3.max.x, 0, 0));
+		Object.assign (this.blocks [2].position, new THREE.Vector3(-box3.max.x * 2, 0, 0));
+		Object.assign (this.blocks [3].position, new THREE.Vector3(-box3.max.x * 3, 0, 0));
+		Object.assign (this.blocks [4].position, new THREE.Vector3(-box3.max.x * 4, 0, 0));
+	}
+	
+	FBX_FILES.forEach(file => {
+		fbxLoader.load(
+		file,
 		(object) => {
 			object.scale.setScalar(0.01);
-			object.rotateX( -Math.PI / 2);
-			scene.add(object)
-		},
-		(xhr) => {
-			console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-		},
-		(error) => {
-			console.log(error)
+			object.rotateX( -Math.PI / 2 );
+			this.blocks.push (object);
+			this.scene.add(object);
+			
+			if (this.blocks.length == FBX_FILES.length) {
+				this.SetupBlocks();
+			}
 		}
 	)
+	});
 	
-	window.addEventListener('resize', onWindowResize, false)
-	function onWindowResize() {
-		camera.aspect = window.innerWidth / window.innerHeight
-		camera.updateProjectionMatrix()
-		renderer.setSize(window.innerWidth, window.innerHeight)
-		render()
+	
+	this.onWindowResize = () => {
+		this.camera.aspect = window.innerWidth / window.innerHeight
+		this.camera.updateProjectionMatrix()
+		this.renderer.setSize(window.innerWidth, window.innerHeight)
+		this.render()
+	}
+	window.addEventListener('resize', this.onWindowResize, false)
+
+	this.stats = new Stats()
+	document.body.appendChild(this.stats.dom)
+	
+	this.MoveAndLeapBlocks = () => {
+		if (this.blockSize) {
+			this.blocks.forEach (block => {
+				block.translateX ( 0.1 );
+				
+				if (block.position.x > this.blockSize.max.x) {
+					block.position.x = -this.blockSize.max.x * 4
+				}
+			});
+		}
+	};
+
+	this.animate = () => {
+		requestAnimationFrame(this.animate)
+
+		this.controls.update()
+		
+		this.MoveAndLeapBlocks();
+
+		this.render()
+
+		this.stats.update()
 	}
 
-	const stats = new Stats()
-	document.body.appendChild(stats.dom)
-
-	function animate() {
-		requestAnimationFrame(animate)
-
-		controls.update()
-
-		render()
-
-		stats.update()
+	this.render = () => {
+		this.renderer.render(this.scene, this.camera)
 	}
-
-	function render() {
-		renderer.render(scene, camera)
-	}
-
-	animate()
+	
+	this.totalTime_ = 0.0;
+    this.previousRAF_ = null;
+	this.scene.fog = new THREE.FogExp2(0xff33d2, 0.05);
+	
+	this.animate()
 }
 
-window.FogHornBoot = FogHornBoot;
+window.app = new FogHornBoot();
